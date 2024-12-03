9da629a74fcc2cff415a5c940ec50e30421078e1
Thandeka325 <marhwayiza325@gmail.com>
2024-12-03 15:33:03 -0800

    Task 14: Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,7 +6,7 @@
  *
  * Return:The height of the tree or 0 if tree is NULL
  */
-int binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
