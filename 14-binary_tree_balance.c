566f3a67057a9894a5fad17ab2174d3504b77110
Thandeka325 <marhwayiza325@gmail.com>
2024-12-03 16:43:45 -0800

    Task 14: Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,13 +8,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t left_height = 0, right_height = 0;
 
-	size_t left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-	size_t right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
+		left_height = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 
-	return (left_height > right_height ? left_height : right_height);
+		return ((left_height > right_height) ? left_height : right_height);
 }
 
 /**
@@ -25,11 +26,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	int left_height = tree->left ? (int)binary_tree_height(tree->left) + 1 : 0;
-	int right_height = tree->right ? (int)binary_tree_height(tree->right) + 1 : 0;
-
-	return (left_height - right_height);
+	if (tree)
+		return (binary_tree_height(tree->left_height) -
+				binary_tree_height(tree->right_height));
 }
