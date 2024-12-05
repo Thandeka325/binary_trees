503970d96817c154776f8ea01c4a114c469fe8e1
Thandeka325 <marhwayiza325@gmail.com>
2024-12-05 15:35:40 -0800

    Task 40: Heap - Sort

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,82 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - Measures the size of a binary tree.
- * @tree: Pointer to the root node of the tree.
+ * heap_to_sorted_array - Converts a Binary Max Heap to a sorted array-integer.
  *
- * Return: Size of the tree (number of nodes).
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
-}
-
-/**
- * heap_extract - Extracts the root node of a Max Binary Heap.
- * @root: Double pointer to the root node of the heap.
+ * @heap: A pointer to the root node of the heap to convert.
+ * @size: An address to store the size of the array.
  *
- * Return: The value stored in the root node, or 0 on failure.
- */
-int heap_extract(heap_t **root)
-{
-	heap_t *last_node;
-	int value;
-	size_t size;
-
-	if (!root || !*root)
-		return (0);
-
-	size = binary_tree_size(*root);
-
-	if (size == 1)
-	{
-		value = (*root)->n;
-		free(*root);
-		*root = NULL;
-		return (value);
-	}
-	last_node = get_last_node(*root, size);
-	value = (*root)->n;
-	(*root)->n = last_node->n;
-
-	if (last_node->parent && last_node->parent->left == last_node)
-		last_node->parent->left = NULL;
-	else if (last_node->parent)
-		last_node->parent->right = NULL;
-	free(last_node);
-
-	heapify_down(*root);
-	return (value);
-}
-
-/**
- * heap_to_sorted_array - Converts a Max Binary Heap to a sorted array.
- * @heap: Pointer to the root of the heap.
- * @size: Pointer to store the size of the array.
- *
- * Return: Pointer to the sorted array, or NULL on failure.
+ * Return: Sorted in descending order
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
 	int *array;
-	size_t i = 0;
-	int value;
+	int extract, i = 0;
+	size_t heap_size;
 
-	if (!heap || !size)
+	if (!heap)
 		return (NULL);
-
-	*size = binary_tree_size(heap);
-	array = malloc(*size * sizeof(int));
-
+	heap_size = binary_tree_size(heap);
+	*size = heap_size;
+	array = malloc(heap_size * sizeof(int));
 	if (!array)
 		return (NULL);
-
 	while (heap)
 	{
-		value = heap_extract(&heap);
-		array[i++] = value;
+		extract = heap_extract(&heap);
+		array[i] = extract;
+		i++;
 	}
 	return (array);
 }
